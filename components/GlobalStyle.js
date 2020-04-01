2530a9b11a3d90394852befd11a3a0c48d088cd3
Bruno Henrique Ferreira <ferreirabrunodev@gmail.com>
2020-03-31 22:39:32 -0300

    GlobalStyle component

---
+++ components/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    background-color: #DDD;
+  }
+`;
+
+export default GlobalStyle;
